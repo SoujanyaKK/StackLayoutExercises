71c71d088a27ced03d57eec2bd2abcc3e9ca9885
skulkarni207 <skulkarni207@IN-4H3K9S3>
2024-02-15 15:26:50 +0530

    Add project files.

---
+++ StackLayoutExercise/StackLayoutExercise/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace StackLayoutExercise
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			//MainPage = new Exercise1();
+			MainPage = new Exercise2();
+		}
+
+		protected override void OnStart()
+		{
+		}
+
+		protected override void OnSleep()
+		{
+		}
+
+		protected override void OnResume()
+		{
+		}
+	}
+}
